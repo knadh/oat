ceee09acd38f7b30735f873557c91b4fec047cd5
Laurent Bardin <laurent.bardin+gitlab@protonmail.com>
2026-02-19 19:40:43 +0100

    Skip replaced elements when initialising tooltips
    Tooltips use `::before` and `::after`, which cannot work on replaced
    elements, the most common of which is `<img>`. We skip those elements
    when initialising the tooltips so that their `title` attribute is not
    removed and it can still be displayed by the browser when hovering over
    them.

--- src/js/tooltip.js
+++ src/js/tooltip.js
@@ -5,14 +5,31 @@
  */
 
 document.addEventListener('DOMContentLoaded', () => {
+  // https://developer.mozilla.org/en-US/docs/Glossary/Replaced_elements
+  const replacedElements = [
+    'img', 'video', 'embed',
+    'iframe', 'fencedframe',
+    // The following three elements are only sometimes considered replaced, but
+    // we'll skip them nonetheless to at least get the native browser tooltip
+    // if they can have it.
+    'audio', 'canvas', 'object',
+  ];
+  const isInputImage = el => el.localName == 'input' && el.type == 'image';
+
   document.querySelectorAll('[title]').forEach(el => {
-    const text = el.getAttribute('title');
-    if (text) {
-      el.setAttribute('data-tooltip', text);
-      if (!el.hasAttribute('aria-label')) {
-        el.setAttribute('aria-label', text);
+    if (
+      !replacedElements.includes(el.localName)
+      &&
+      !isInputImage(el)
+    ) {
+      const text = el.getAttribute('title');
+      if (text) {
+        el.setAttribute('data-tooltip', text);
+        if (!el.hasAttribute('aria-label')) {
+          el.setAttribute('aria-label', text);
+        }
+        el.removeAttribute('title');
       }
-      el.removeAttribute('title');
     }
   });
 });
